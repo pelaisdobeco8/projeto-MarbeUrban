d26d3227784480445da5f6b151ae661c03da4914
Bea <47746451+pelaisdobeco8@users.noreply.github.com>
2019-05-22 15:04:29 -0300

    Update clienteControlador.php

--- MarbeUrban/controlador/clienteControlador.php
+++ MarbeUrban/controlador/clienteControlador.php
@@ -9,11 +9,37 @@ function adicionar () {
 		$confirma = $_POST ["confirma"];
 		$sexo = $_POST ["sexo"];
 		
-		echo $nome . "<br>" ;
-		echo $sobnome . "<br>";
-		echo $emailn ."<br>";	
-		echo $senha ."<br>";
-		echo $confirma . "<br>";
+		if (strlen(trim($_POST['nome'])) == 0){
+                echo "Você precisa registrar seu nome. <br>";
+            }else {
+                echo $nome . "<br>" ;
+            }
+            
+            if (strlen(trim($_POST['sobnome'])) == 0){
+               echo "Você precisa informar seu sobrenome. <br>";
+            }else {
+                echo $sobnome . "<br>";
+            }
+            
+            if (strlen(trim($_POST['emailn'])) == 0){
+               echo "Você precisa registrar seu e-mail. <br>"; 
+            }else {
+                echo $emailn ."<br>";
+            }
+            
+            if (strlen(trim($_POST['senha'])) == 0){
+                echo "Você precisa criar uma senha. <br>";
+            }else{
+                echo $senha ."<br>";
+            }
+            
+             if (strlen(trim($_POST['confirma'])) != 'senha'){
+                 echo "A senha não é compatível. <br>";
+             }else{
+                 echo $confirma . "<br>";
+             }
+		
+		echo $sexo;
 	}else {
 		exibir ("cliente/formulario");
 	}
